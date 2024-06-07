8111ced42c00b42a1977085d24c37c4f378e4572
HazemGhanem88 <160427047+HazemGhanem88@users.noreply.github.com>
2024-06-07 18:57:34 +0300

    first

--- databases/dbconnection.js
+++ databases/dbconnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 export const dbConnection = () => {
-    mongoose.connect("mongodb+srv://admin:4KxjXIicF9XOGH1b@cluster0.5mhmytf.mongodb.net/robbikya").then(() => {
+    mongoose.connect("mongodb+srv://admin:Rq9K2Hqnlw6HzBLI@cluster0.5mhmytf.mongodb.net/robbikya").then(() => {
         console.log('database connected');
     }).catch((err) => {
         console.log('databases error', err);
