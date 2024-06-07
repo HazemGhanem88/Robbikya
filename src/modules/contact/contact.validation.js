318bec62a86bfffd875b56d225e9e931c4b3f372
HazemGhanem88 <160427047+HazemGhanem88@users.noreply.github.com>
2024-06-07 19:44:33 +0300

    first

--- src/modules/contact/contact.validation.js
+++ src/modules/contact/contact.validation.js
@@ -5,7 +5,7 @@ const addcontactval = Joi.object({
     
     Name: Joi.string().min(2).max(100).required().trim(),
     email: Joi.string().pattern(/[A-Za-z0-9]{3,50}@(gmail|yahoo).com$/),
-    message:Joi.string().min(50).max(1500).trim(),
+    message:Joi.string().min(30).max(1500).trim(),
 
 })
 
